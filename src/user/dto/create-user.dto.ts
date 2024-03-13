163407001c6c6b0f0e025d2777638612fe4755b9
Fiido <roccobosio@gmail.com>
2024-03-13 20:04:29 -0300

    user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,32 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateUserDto {
+  @ApiProperty({
+    name: "name",
+    example: "Bruce",
+    type: "string",
+    required: true,
+  })
   name: string
+  @ApiProperty({
+    name: "surname",
+    example: "Wayne",
+    type: "string",
+    required: true,
+  })
   surname: string
+  @ApiProperty({
+    name: "email",
+    example: "bwayne@gmail.com",
+    type: "string",
+    required: true,
+  })
   email: string
+  @ApiProperty({
+    name: "password",
+    example: "123456",
+    type: "string",
+    required: true,
+  })
   password: string
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class UpdateUserDto {
+  @ApiProperty({
+    name: "name",
+    example: "John",
+    type: "string",
+    required: false,
+  })
   name?: string
+  @ApiProperty({
+    name: "surname",
+    example: "Doe",
+    type: "string",
+    required: false,
+  })
   surname?: string
+  @ApiProperty({
+    name: "password",
+    example: "123456",
+    type: "string",
+    required: false,
+  })
   password?: string
 }
