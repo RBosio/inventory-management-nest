10a4957d7e1bb245171fa190b1736a298a97732f
Fiido <roccobosio@gmail.com>
2024-03-13 18:08:58 -0300

    signup and login endpoints

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+import { BadRequestException, Injectable } from "@nestjs/common"
+import { User } from "src/entities/user.entity"
+import { CreateUserDto } from "src/user/dto/create-user.dto"
+import { UserService } from "src/user/user.service"
+import { JwtService } from "@nestjs/jwt"
+import { LoginUserDto } from "./dto/login-user.dto"
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(createUserDto: CreateUserDto): Promise<User> {
+    return this.userService.create(createUserDto)
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const userFounded = await this.userService.findOneByEmail(
+      loginUserDto.email,
+    )
+
+    if (userFounded.password !== loginUserDto.password)
+      throw new BadRequestException("email or password wrong")
+
+    const payload = { sub: userFounded.id }
+
+    return {
+      token: await this.jwtService.signAsync(payload),
+    }
+  }
+}
