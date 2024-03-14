b328b43099635253c90d0b7ec275cb3628e6d059
Fiido <roccobosio@gmail.com>
2024-03-13 23:01:34 -0300

    auth dto

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class LoginUserDto {
+  @ApiProperty({
+    name: "email",
+    type: "string",
+    example: "bwayne@gmail.com",
+    required: true,
+  })
   email: string
+
+  @ApiProperty({
+    name: "password",
+    type: "string",
+    example: "123456",
+    required: true,
+  })
   password: string
 }
