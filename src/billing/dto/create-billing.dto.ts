fb0ed4a6af1cf915cc9cca0a7a342be09d95d8eb
Fiido <roccobosio@gmail.com>
2024-03-13 21:44:26 -0300

    billing dto

--- src/billing/dto/create-billing.dto.ts
+++ src/billing/dto/create-billing.dto.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger"
+
 export class CreateBillingDto {
+  @ApiProperty({
+    name: "userId",
+    type: "number",
+    example: 1,
+    required: true,
+  })
   userId: number
+
+  @ApiProperty({
+    name: "customerId",
+    type: "number",
+    example: 2,
+    required: true,
+  })
   customerId: number
 }
